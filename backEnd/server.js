1b401d0044d343d40fe370502f92abf2e16ac114
sahilshahu555 <sahilshahu555@gmail.com>
2023-08-16 22:46:42 +0530

    edited

--- backEnd/server.js
+++ backEnd/server.js
@@ -2,7 +2,7 @@ const app = require("./index");
 const connect = require("./configs/db");
 require("dotenv").config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, async (req, res) => {
   
