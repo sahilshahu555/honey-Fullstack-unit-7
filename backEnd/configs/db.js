100e1ce598313c30a86605a49409f3ae2ef5f21d
Sahil Shahu <125876375+sahilshahu555@users.noreply.github.com>
2023-08-16 23:24:41 +0530

    Edited

--- backEnd/configs/db.js
+++ backEnd/configs/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const URL =process.env.MONGO_URI ||
-  "mongodb+srv://rawatmegha9354:PYduvzClwLiJoJsU@cluster0.jlkodts.mongodb.net/project_backend";
+  "mongodb+srv://sahilshahu9354:PYduvzClwLiJoJsU@cluster0.jlkodts.mongodb.net/project_backend";
 module.exports = () => {
   return mongoose.connect(URL);
 };
